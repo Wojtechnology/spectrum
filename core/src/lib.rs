f03e98df19655e5c742eb81ddb8e2c3e981e8042
Wojtek Swiderski <wswiders@uwaterloo.ca>
2019-07-12 00:17:39 -0400

    Reading mp3 files

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,7 +1,71 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate minimp3;
+
+use std::io;
+use std::io::Read;
+
+#[derive(Debug)]
+pub enum Error {
+    Io(io::Error),
+    IllFormed,
+    Application(&'static str),
+    Empty,
+}
+
+pub mod mp3 {
+    use minimp3::{Decoder, Error as Mp3Error, Frame};
+
+    use super::{Error, Read};
+
+    pub struct Mp3Decoder<R> {
+        decoder: Decoder<R>,
+        current_frame: Frame,
+        current_frame_offset: usize,
+    }
+
+    impl<R> Mp3Decoder<R>
+    where
+        R: Read,
+    {
+        pub fn new(reader: R) -> Result<Mp3Decoder<R>, Error> {
+            let mut decoder = Decoder::new(reader);
+            let current_frame = loop {
+                match decoder.next_frame() {
+                    Ok(frame) => break Ok(frame),
+                    Err(Mp3Error::Io(err)) => break Err(Error::Io(err)),
+                    Err(Mp3Error::InsufficientData) => {
+                        break Err(Error::Application("Insufficient data"))
+                    }
+                    Err(Mp3Error::Eof) => break Err(Error::Empty),
+                    Err(Mp3Error::SkippedData) => continue,
+                }
+            }?;
+
+            Ok(Mp3Decoder {
+                decoder,
+                current_frame,
+                current_frame_offset: 0,
+            })
+        }
+    }
+
+    impl<R> Iterator for Mp3Decoder<R>
+    where
+        R: Read,
+    {
+        type Item = i16;
+
+        fn next(&mut self) -> Option<i16> {
+            if self.current_frame_offset == self.current_frame.data.len() {
+                self.current_frame_offset = 0;
+                match self.decoder.next_frame() {
+                    Ok(frame) => self.current_frame = frame,
+                    Err(_) => return None,
+                }
+            }
+
+            let v = self.current_frame.data[self.current_frame_offset];
+            self.current_frame_offset += 1;
+            Some(v)
+        }
     }
 }
--- stream-gen/src/main.rs
+++ stream-gen/src/main.rs
@@ -1,3 +1,12 @@
+extern crate core;
+
+use std::fs::File;
+
+use core::mp3::Mp3Decoder;
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: Error handling
+    let reader = File::open("/Users/wojtekswiderski/Music/iTunes/iTunes Media/Music/Felix Cartel/Unknown Album/Young Love.mp3").unwrap();
+    let decoder = Mp3Decoder::new(reader).unwrap();
+    for _ in decoder {}
 }
