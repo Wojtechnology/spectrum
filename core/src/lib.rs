108daefeafa36aa14d13e62505a883bd4d0c37a4
Wojtek Swiderski <wswiders@uwaterloo.ca>
2019-07-16 19:45:47 -0400

    Better decoder error

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,7 +1,8 @@
 extern crate minimp3;
 
+use std::error::Error;
+use std::fmt;
 use std::io;
-use std::io::Read;
 
 #[derive(Debug)]
 pub enum DecoderError {
@@ -11,15 +12,45 @@ pub enum DecoderError {
     Empty,
 }
 
+impl fmt::Display for DecoderError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            DecoderError::Io(io_err) => write!(f, "Error when reading file: {}", io_err),
+            DecoderError::Application(s) => write!(f, "Application error: {}", s),
+            DecoderError::IllFormed => write!(f, "File is illformed"),
+            DecoderError::Empty => write!(f, "File is empty"),
+        }
+    }
+}
+
+impl Error for DecoderError {
+    fn description(&self) -> &str {
+        match self {
+            DecoderError::Io(_) => "Error when reading file",
+            DecoderError::Application(_) => "Application error",
+            DecoderError::IllFormed => "File is illformed",
+            DecoderError::Empty => "File is empty",
+        }
+    }
+}
+
+pub trait RawStream<T>: Iterator<Item = T> {
+    fn channels(&self) -> usize;
+    fn sample_rate(&self) -> i32;
+}
+
 pub mod mp3 {
+    use std::io::Read;
+
     use minimp3::{Decoder, Error as Mp3Error, Frame};
 
-    use super::{DecoderError, Read};
+    use super::{DecoderError, RawStream};
 
     pub struct Mp3Decoder<R> {
         decoder: Decoder<R>,
         current_frame: Frame,
         current_frame_offset: usize,
+        frame_index: usize,
     }
 
     impl<R> Mp3Decoder<R>
@@ -30,7 +61,9 @@ pub mod mp3 {
             let mut decoder = Decoder::new(reader);
             let current_frame = loop {
                 match decoder.next_frame() {
-                    Ok(frame) => break Ok(frame),
+                    Ok(frame) => {
+                        break Ok(frame);
+                    }
                     Err(Mp3Error::Io(err)) => break Err(DecoderError::Io(err)),
                     Err(Mp3Error::InsufficientData) => {
                         break Err(DecoderError::Application("Insufficient data"))
@@ -44,6 +77,7 @@ pub mod mp3 {
                 decoder,
                 current_frame,
                 current_frame_offset: 0,
+                frame_index: 0,
             })
         }
     }
@@ -54,11 +88,17 @@ pub mod mp3 {
     {
         type Item = i16;
 
-        fn next(&mut self) -> Option<i16> {
+        fn next(&mut self) -> Option<Self::Item> {
             if self.current_frame_offset == self.current_frame.data.len() {
                 self.current_frame_offset = 0;
                 match self.decoder.next_frame() {
-                    Ok(frame) => self.current_frame = frame,
+                    Ok(frame) => {
+                        // NOTE: We assume that the sample rate and channels of the song stays the
+                        // same throughout. Should add a debug test here to check if this is
+                        // actually the case
+                        self.current_frame = frame;
+                        self.frame_index += 1;
+                    }
                     Err(_) => return None,
                 }
             }
@@ -66,6 +106,19 @@ pub mod mp3 {
             let v = self.current_frame.data[self.current_frame_offset];
             self.current_frame_offset += 1;
             Some(v)
+        }
+    }
+
+    impl<R> RawStream<i16> for Mp3Decoder<R>
+    where
+        R: Read,
+    {
+        fn channels(&self) -> usize {
+            self.current_frame.channels
+        }
+
+        fn sample_rate(&self) -> i32 {
+            self.current_frame.sample_rate
         }
     }
 }
--- stream-gen/src/main.rs
+++ stream-gen/src/main.rs
@@ -5,7 +5,7 @@ use std::fs::File;
 use std::process;
 
 use core::mp3::Mp3Decoder;
-use core::DecoderError;
+use core::RawStream;
 
 fn main() {
     let mut args = env::args();
@@ -19,19 +19,22 @@ fn main() {
         }
     };
 
-    let reader = File::open(filename).unwrap_or_else(|err| {
+    let reader = File::open(&filename).unwrap_or_else(|err| {
         eprintln!("Error when reading file: {}", err);
         process::exit(1);
     });
+
     let decoder = Mp3Decoder::new(reader).unwrap_or_else(|err| {
-        let err_msg = match err {
-            DecoderError::Io(io_err) => format!("Error when reading file: {}", io_err),
-            DecoderError::Application(s) => format!("Application error: {}", s),
-            DecoderError::IllFormed => format!("File is illformed"),
-            DecoderError::Empty => format!("File is empty"),
-        };
-        eprintln!("{}", err_msg);
+        eprintln!("{}", err);
         process::exit(1);
     });
+
+    println!(
+        "Track {}\n\tchannels: {}\n\tsample_rate: {}",
+        &filename,
+        decoder.channels(),
+        decoder.sample_rate()
+    );
+
     for _ in decoder {}
 }
