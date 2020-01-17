82b7279caf4669ccfa0772682faeba6a34df58d8
Wojtek Swiderski <wswiders@uwaterloo.ca>
2020-01-16 22:53:47 -0800

    Cleaned up event loop

--- spectrum-viz/src/main.rs
+++ spectrum-viz/src/main.rs
@@ -10,28 +10,21 @@ extern crate gfx_backend_metal as back;
 #[cfg(feature = "vulkan")]
 extern crate gfx_backend_vulkan as back;
 
-#[macro_use]
-extern crate log;
 extern crate env_logger;
 extern crate gfx_hal as hal;
 extern crate glsl_to_spirv;
 extern crate image;
+extern crate log;
 extern crate winit;
 
 use spectrum_viz::backend_state::create_backend;
 use spectrum_viz::renderer_state::RendererState;
 use spectrum_viz::screen_size_state::ScreenSizeState;
 
-#[derive(Debug)]
-enum Color {
-    Red,
-    Green,
-    Blue,
-    Alpha,
-}
-
-#[cfg(any(feature = "vulkan", feature = "metal",))]
-fn main() {
+fn init() -> (
+    winit::event_loop::EventLoop<()>,
+    RendererState<back::Backend>,
+) {
     env_logger::init();
 
     let event_loop = winit::event_loop::EventLoop::new();
@@ -43,165 +36,53 @@ fn main() {
         .with_title("colour-uniform".to_string());
     let backend = create_backend(wb, &event_loop);
 
-    let mut renderer_state = unsafe { RendererState::new(backend) };
+    let renderer_state = unsafe { RendererState::new(backend) };
+    (event_loop, renderer_state)
+}
 
-    let mut r = 1.0f32;
-    let mut g = 1.0f32;
-    let mut b = 1.0f32;
-    let mut a = 1.0f32;
+#[cfg(any(feature = "vulkan", feature = "metal",))]
+fn main() {
+    let (event_loop, mut renderer_state) = init();
 
-    let mut cr = 0.8;
-    let mut cg = 0.8;
-    let mut cb = 0.8;
-
+    let cr = 0.8;
+    let cg = 0.8;
+    let cb = 0.8;
     let mut x = 0.5;
     let mut y = 0.5;
-
-    let mut cur_color = Color::Red;
-    let mut cur_value: u32 = 0;
-
-    println!("\nInstructions:");
-    println!("\tChoose whether to change the (R)ed, (G)reen or (B)lue color by pressing the appropriate key.");
-    println!("\tType in the value you want to change it to, where 0 is nothing, 255 is normal and 510 is double, ect.");
-    println!("\tThen press C to change the (C)lear colour or (Enter) for the image color.");
-    println!(
-        "\tSet {:?} color to: {} (press enter/C to confirm)",
-        cur_color, cur_value
-    );
 
     renderer_state.draw_triangle(cr, cg, cb);
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = winit::event_loop::ControlFlow::Poll;
-        let uniform = &mut renderer_state.uniform;
         let viewport = &mut renderer_state.viewport;
         match event {
-            winit::event::Event::WindowEvent { event, .. } =>
-            {
-                #[allow(unused_variables)]
-                match event {
-                    winit::event::WindowEvent::KeyboardInput {
-                        input:
-                            winit::event::KeyboardInput {
-                                virtual_keycode: Some(winit::event::VirtualKeyCode::Escape),
-                                ..
-                            },
-                        ..
-                    }
-                    | winit::event::WindowEvent::CloseRequested => {
-                        *control_flow = winit::event_loop::ControlFlow::Exit
-                    }
-                    winit::event::WindowEvent::Resized(size) => {
-                        renderer_state.resize(size);
-                    }
-                    winit::event::WindowEvent::HiDpiFactorChanged(dpi_factor) => {
-                        renderer_state.change_dpi(dpi_factor);
-                    }
-                    winit::event::WindowEvent::RedrawRequested => {
-                        renderer_state.draw_triangle(cr, cg, cb);
-                    }
-                    winit::event::WindowEvent::KeyboardInput {
-                        input:
-                            winit::event::KeyboardInput {
-                                virtual_keycode,
-                                state: winit::event::ElementState::Pressed,
-                                ..
-                            },
-                        ..
-                    } => {
-                        if let Some(kc) = virtual_keycode {
-                            match kc {
-                                winit::event::VirtualKeyCode::Key0 => {
-                                    cur_value = cur_value * 10 + 0
-                                }
-                                winit::event::VirtualKeyCode::Key1 => {
-                                    cur_value = cur_value * 10 + 1
-                                }
-                                winit::event::VirtualKeyCode::Key2 => {
-                                    cur_value = cur_value * 10 + 2
-                                }
-                                winit::event::VirtualKeyCode::Key3 => {
-                                    cur_value = cur_value * 10 + 3
-                                }
-                                winit::event::VirtualKeyCode::Key4 => {
-                                    cur_value = cur_value * 10 + 4
-                                }
-                                winit::event::VirtualKeyCode::Key5 => {
-                                    cur_value = cur_value * 10 + 5
-                                }
-                                winit::event::VirtualKeyCode::Key6 => {
-                                    cur_value = cur_value * 10 + 6
-                                }
-                                winit::event::VirtualKeyCode::Key7 => {
-                                    cur_value = cur_value * 10 + 7
-                                }
-                                winit::event::VirtualKeyCode::Key8 => {
-                                    cur_value = cur_value * 10 + 8
-                                }
-                                winit::event::VirtualKeyCode::Key9 => {
-                                    cur_value = cur_value * 10 + 9
-                                }
-                                winit::event::VirtualKeyCode::R => {
-                                    cur_value = 0;
-                                    cur_color = Color::Red
-                                }
-                                winit::event::VirtualKeyCode::G => {
-                                    cur_value = 0;
-                                    cur_color = Color::Green
-                                }
-                                winit::event::VirtualKeyCode::B => {
-                                    cur_value = 0;
-                                    cur_color = Color::Blue
-                                }
-                                winit::event::VirtualKeyCode::A => {
-                                    cur_value = 0;
-                                    cur_color = Color::Alpha
-                                }
-                                winit::event::VirtualKeyCode::Return => {
-                                    match cur_color {
-                                        Color::Red => r = cur_value as f32 / 255.0,
-                                        Color::Green => g = cur_value as f32 / 255.0,
-                                        Color::Blue => b = cur_value as f32 / 255.0,
-                                        Color::Alpha => a = cur_value as f32 / 255.0,
-                                    }
-                                    uniform
-                                        .buffer
-                                        .as_mut()
-                                        .unwrap()
-                                        .update_data(0, &[r, g, b, a]);
-                                    cur_value = 0;
-
-                                    println!("Colour updated!");
-                                }
-                                winit::event::VirtualKeyCode::C => {
-                                    match cur_color {
-                                        Color::Red => cr = cur_value as f32 / 255.0,
-                                        Color::Green => cg = cur_value as f32 / 255.0,
-                                        Color::Blue => cb = cur_value as f32 / 255.0,
-                                        Color::Alpha => {
-                                            error!("Alpha is not valid for the background.");
-                                            return;
-                                        }
-                                    }
-                                    cur_value = 0;
-
-                                    println!("Background color updated!");
-                                }
-                                _ => return,
-                            }
-                            println!(
-                                "Set {:?} color to: {} (press enter/C to confirm)",
-                                cur_color, cur_value
-                            )
-                        }
-                    }
-                    winit::event::WindowEvent::CursorMoved { position, .. } => {
-                        x = (position.x as f32) / (viewport.rect.w as f32) * 4.0 - 1.0;
-                        y = (position.y as f32) / (viewport.rect.h as f32) * 4.0 - 1.0;
-                    }
-                    _ => (),
+            winit::event::Event::WindowEvent { event, .. } => match event {
+                winit::event::WindowEvent::KeyboardInput {
+                    input:
+                        winit::event::KeyboardInput {
+                            virtual_keycode: Some(winit::event::VirtualKeyCode::Escape),
+                            ..
+                        },
+                    ..
                 }
-            }
+                | winit::event::WindowEvent::CloseRequested => {
+                    *control_flow = winit::event_loop::ControlFlow::Exit
+                }
+                winit::event::WindowEvent::Resized(size) => {
+                    renderer_state.resize(size);
+                }
+                winit::event::WindowEvent::HiDpiFactorChanged(dpi_factor) => {
+                    renderer_state.change_dpi(dpi_factor);
+                }
+                winit::event::WindowEvent::RedrawRequested => {
+                    renderer_state.draw_triangle(cr, cg, cb);
+                }
+                winit::event::WindowEvent::CursorMoved { position, .. } => {
+                    x = (position.x as f32) / (viewport.rect.w as f32) * 4.0 - 1.0;
+                    y = (position.y as f32) / (viewport.rect.h as f32) * 4.0 - 1.0;
+                }
+                _ => (),
+            },
             winit::event::Event::EventsCleared => {
                 renderer_state.backend.window.request_redraw();
             }
