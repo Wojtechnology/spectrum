3aa5d9866c127fac2ca22595382c49211e4bd00f
Wojtek Swiderski <wswiders@uwaterloo.ca>
2020-01-28 22:44:38 -0800

    Move audio loop into module

--- spectrum-audio/src/lib.rs
+++ spectrum-audio/src/lib.rs
@@ -1,8 +1,9 @@
-extern crate minimp3;
-
 use std::error::Error;
 use std::fmt;
 use std::io;
+
+use cpal::traits::{DeviceTrait, EventLoopTrait, HostTrait};
+use cpal::{Sample, StreamData, UnknownTypeOutputBuffer};
 
 #[derive(Debug)]
 pub enum DecoderError {
@@ -121,4 +122,73 @@ pub mod mp3 {
             self.current_frame.sample_rate
         }
     }
+}
+
+pub fn run_audio_loop<D: RawStream<i16>>(decoder: D) {
+    let host = cpal::default_host();
+    let event_loop = host.event_loop();
+    // TODO: Error processing
+    let device = host
+        .default_output_device()
+        .expect("no output device available");
+    let mut supported_formats_range = device
+        .supported_output_formats()
+        .expect("error while querying_formats");
+    supported_formats_range.next();
+    let format = supported_formats_range
+        .next()
+        .expect("no supported format")
+        .with_max_sample_rate();
+    let stream_id = event_loop.build_output_stream(&device, &format).unwrap();
+    event_loop
+        .play_stream(stream_id)
+        .expect("failed to play_stream");
+    let vals: Vec<i16> = decoder.collect();
+    let mut v_iter = vals.iter();
+    event_loop.run(move |stream_id, stream_result| {
+        let stream_data = match stream_result {
+            Ok(data) => data,
+            Err(err) => {
+                eprintln!("an error occurred on stream {:?}: {}", stream_id, err);
+                return;
+            }
+        };
+
+        match stream_data {
+            StreamData::Output {
+                buffer: UnknownTypeOutputBuffer::U16(mut buffer),
+            } => {
+                for elem in buffer.iter_mut() {
+                    let v = match v_iter.next() {
+                        Some(&v) => v.to_u16(),
+                        None => u16::max_value() / 2,
+                    };
+                    *elem = v;
+                }
+            }
+            StreamData::Output {
+                buffer: UnknownTypeOutputBuffer::I16(mut buffer),
+            } => {
+                for elem in buffer.iter_mut() {
+                    let v = match v_iter.next() {
+                        Some(&v) => v,
+                        None => 0,
+                    };
+                    *elem = v;
+                }
+            }
+            StreamData::Output {
+                buffer: UnknownTypeOutputBuffer::F32(mut buffer),
+            } => {
+                for elem in buffer.iter_mut() {
+                    let v = match v_iter.next() {
+                        Some(&v) => v.to_f32(),
+                        None => 0.0,
+                    };
+                    *elem = v;
+                }
+            }
+            _ => (),
+        }
+    });
 }
--- viz/src/main.rs
+++ viz/src/main.rs
@@ -1,84 +1,9 @@
-extern crate cpal;
-extern crate spectrum_audio;
-
 use std::env;
 use std::fs::File;
 use std::process;
 
 use spectrum_audio::mp3::Mp3Decoder;
-use spectrum_audio::RawStream;
-
-use cpal::traits::{DeviceTrait, EventLoopTrait, HostTrait};
-use cpal::{Sample, StreamData, UnknownTypeOutputBuffer};
-
-fn run_audio_loop<D: RawStream<i16>>(decoder: D) {
-    let host = cpal::default_host();
-    let event_loop = host.event_loop();
-    // TODO: Error processing
-    let device = host
-        .default_output_device()
-        .expect("no output device available");
-    let mut supported_formats_range = device
-        .supported_output_formats()
-        .expect("error while querying_formats");
-    supported_formats_range.next();
-    let format = supported_formats_range
-        .next()
-        .expect("no supported format")
-        .with_max_sample_rate();
-    let stream_id = event_loop.build_output_stream(&device, &format).unwrap();
-    event_loop
-        .play_stream(stream_id)
-        .expect("failed to play_stream");
-    let vals: Vec<i16> = decoder.collect();
-    let mut v_iter = vals.iter();
-    event_loop.run(move |stream_id, stream_result| {
-        let stream_data = match stream_result {
-            Ok(data) => data,
-            Err(err) => {
-                eprintln!("an error occurred on stream {:?}: {}", stream_id, err);
-                return;
-            }
-        };
-
-        match stream_data {
-            StreamData::Output {
-                buffer: UnknownTypeOutputBuffer::U16(mut buffer),
-            } => {
-                for elem in buffer.iter_mut() {
-                    let v = match v_iter.next() {
-                        Some(&v) => v.to_u16(),
-                        None => u16::max_value() / 2,
-                    };
-                    *elem = v;
-                }
-            }
-            StreamData::Output {
-                buffer: UnknownTypeOutputBuffer::I16(mut buffer),
-            } => {
-                for elem in buffer.iter_mut() {
-                    let v = match v_iter.next() {
-                        Some(&v) => v,
-                        None => 0,
-                    };
-                    *elem = v;
-                }
-            }
-            StreamData::Output {
-                buffer: UnknownTypeOutputBuffer::F32(mut buffer),
-            } => {
-                for elem in buffer.iter_mut() {
-                    let v = match v_iter.next() {
-                        Some(&v) => v.to_f32(),
-                        None => 0.0,
-                    };
-                    *elem = v;
-                }
-            }
-            _ => (),
-        }
-    });
-}
+use spectrum_audio::run_audio_loop;
 
 fn main() {
     let mut args = env::args();
